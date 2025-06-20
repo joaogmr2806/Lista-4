b430cc323e8bb608f8d6b2d7ba3bf77590a30822
joaogmr2806 <jgabrielmaiarodrigues@gmail.com>
2025-06-19 23:13:21 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import ipeadatapy as ip
 
-# 1)
+
 st.set_page_config(page_title="Lista de Exercícios 4")
 st.title("Projeto Final – Análise Contábil com Ajuste Econômico")
 st.write("Este projeto tem como objetivo integrar análise de dados contábeis de empresas com indicadores econômicos, utilizando Python, Pandas, Ipeadata e Streamlit.")
